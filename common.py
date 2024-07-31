40974d363b339cadc89e5e1cec1a2c86254a4513
Alyna Gray <alynagray1@gmail.com>
2024-07-30 21:23:29 -0700

    Reduce detection chance

--- common.py
+++ common.py
@@ -4,9 +4,8 @@ from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 import urllib.request
 import os
-from typing import Iterator
+import random
 
-USER_AGENT = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
 BASE_URL = "https://android.magi-reco.com/"
 # The game server always returns the same html page with status code 200 is the asset is not found
 NONEXISTENT_ASSET = "magica/resource/nonexistent_asset"
@@ -14,15 +13,25 @@ NONEXISTENT_ASSET_PAGE_FILENAME = "nonexistent_asset_page.html"
 with open(NONEXISTENT_ASSET_PAGE_FILENAME, mode="rb") as file:
     NONEXISTENT_ASSET_PAGE = file.read()
 
+HEADERS = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0",
+           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8",
+           "Accept-Language": "en-US,en;q=0.5",
+           "Accept-Encoding": "gzip, deflate",
+           "DNT": "1",
+           "Connection": "keep-alive",
+           "Upgrade-Insecure-Requests": "1",
+           "Priority": "u=0, i",
+           "Pragma": "no-cache",
+           "Cache-Control": "no-cache"}
+
 GREEN = "\033[0;32m"
 RESET = "\033[0m"
 
 def scrape_all(candidates: callable) -> None:
     if not nonexistent_asset_page_correct():
-        print(f"Error: {NONEXISTENT_ASSET_PAGE_FILENAME} not correct page")
         exit(1)
     
-    with ThreadPoolExecutor(max_workers=50) as executor:
+    with ThreadPoolExecutor(max_workers=1) as executor:
         for asset in candidates():
             while True:
                 try:
@@ -30,14 +39,14 @@ def scrape_all(candidates: callable) -> None:
                     executor.submit(scrape(asset))
                     break
                 except Exception as e:
-                    print(f"{type(e): {e}}")
+                    print(e)
 
 
 def scrape(asset: str) -> None:
     # asset is the portion after the domain, without the leading /
     # e.g. "magica/resource/image_web/memoria/memoria_1003_s.png"
-    req = urllib.request.Request(f"{BASE_URL}{asset}", headers={"User-Agent": USER_AGENT})
-    with urllib.request.urlopen(req) as response:
+    req = urllib.request.Request(f"{BASE_URL}{asset}", headers=HEADERS)
+    with urllib.request.urlopen(req, timeout=random.randint(4,10)) as response:
         if response.status != 200:
             print(f"Returned status code {response.status}")
         else:
@@ -45,16 +54,22 @@ def scrape(asset: str) -> None:
             content = response.read()
             if content == NONEXISTENT_ASSET_PAGE:
                 return
+            print(f"{GREEN}Found {asset}!{RESET}")
+            os.makedirs(os.path.dirname(Path(asset)), exist_ok=True)
             with open(Path(asset), mode="wb") as file:
-                print(f"{GREEN}Found {asset}!{RESET}")
-                os.makedirs(os.path.dirname(Path(asset)), exist_ok=True)
-                file.write(response.read())
+                file.write(content)
 
 def nonexistent_asset_page_correct() -> bool:
-    req = urllib.request.Request(f"{BASE_URL}{NONEXISTENT_ASSET}", headers={"User-Agent": USER_AGENT})
+    req = urllib.request.Request(f"{BASE_URL}{NONEXISTENT_ASSET}", headers=HEADERS)
     with urllib.request.urlopen(req) as response:
         if response.status != 200:
             print(f"Returned status code {response.status}")
             return False
         else:
-            return NONEXISTENT_ASSET_PAGE == response.read()
+            content = response.read()
+            if NONEXISTENT_ASSET_PAGE != content:
+                print(f"Error: {NONEXISTENT_ASSET_PAGE_FILENAME} not correct page")
+                print(f"Actual response: \"{NONEXISTENT_ASSET_PAGE}\"")
+                return False
+            else:
+                return True
