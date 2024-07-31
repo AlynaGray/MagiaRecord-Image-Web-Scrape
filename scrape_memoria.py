9b68eff8733734e51fa05dc0eaef5878cb1d2800
Alyna Gray <alynagray1@gmail.com>
2024-07-30 21:24:12 -0700

    Change memoria number range

--- scrape_memoria.py
+++ scrape_memoria.py
@@ -6,7 +6,7 @@ import common
 def memoria_candidates() -> Generator[str, None, None]:
     part1 = "magica/resource/image_web/memoria/memoria_"
     part3 = "_s.png"
-    for i in range(10000):
+    for i in range(1000, 10000):
         part2 = str(i).zfill(4)
         yield f"{part1}{part2}{part3}"
 
