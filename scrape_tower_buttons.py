51a316b2c619befb8349b6af1c64c353094c113a
Alyna Gray <alynagray1@gmail.com>
2024-07-30 21:08:10 -0700

    Scrape tower buttons

--- scrape_tower_buttons.py
+++ scrape_tower_buttons.py
@@ -6,11 +6,11 @@ import common
 def tower_button_candidates() -> Generator[str, None, None]:
     p1 = "magica/resource/image_web/event/tower/"
     p3 = "/tab_"
-    for i in range(10000):
+    for i in range(1000, 10000):
         p2 = str(i).zfill(4)
         for p4 in ["normal", "challenge"]:
-            for part5 in ["_on.png", "_off.png"]:
-        yield f"{p1}{p2}{p3}{p4}"
+            for p5 in ["_on.png", "_off.png"]:
+                yield f"{p1}{p2}{p3}{p4}{p5}"
 
 def main():
     common.scrape_all(tower_button_candidates)
