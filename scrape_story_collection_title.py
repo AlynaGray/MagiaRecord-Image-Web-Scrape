125417bb6102b1fe90fb4c934e5b9c0356a25d6a
Alyna Gray <alynagray1@gmail.com>
2024-10-13 04:50:10 -0700

    Uncomment scrape story collection title
    I commented the earlier sections out because I messed up the later
    sections while in development, and I didn't notice until after scraping.
    I didn't want to repeat the earlier sections after fixing it.

--- scrape_story_collection_title.py
+++ scrape_story_collection_title.py
@@ -5,25 +5,25 @@ from itertools import chain
 import common
 
 def candidates() -> Generator[str, None, None]:
-    # yield "magica/resource/image_web/page/quest/puellaHistoria_lastBattle/event/1198/story_collection_title.png"
+    yield "magica/resource/image_web/page/quest/puellaHistoria_lastBattle/event/1198/story_collection_title.png"
 
-    # p1 = "magica/resource/image_web/event/"
-    # p3 = "/"
-    # p5 = "/story_collection_title.png"
-    # for i in ("accomplish", "aprilfool2018", "arenaMission", "arenaRankMatch", "arenaranking", "branch", "dailytower", "dungeon", "eventWalpurgis", "eventWitch", "raid", "singleraid", "storyraid", "tower", "training"):
-    #     p2 = i
-    #     for j in range(1000, 1401):
-    #         p4 = str(j).zfill(4)
-    #         yield f"{p1}{p2}{p3}{p4}{p5}"
+    p1 = "magica/resource/image_web/event/"
+    p3 = "/"
+    p5 = "/story_collection_title.png"
+    for i in ("accomplish", "aprilfool2018", "arenaMission", "arenaRankMatch", "arenaranking", "branch", "dailytower", "dungeon", "eventWalpurgis", "eventWitch", "raid", "singleraid", "storyraid", "tower", "training"):
+        p2 = i
+        for j in range(1000, 1401):
+            p4 = str(j).zfill(4)
+            yield f"{p1}{p2}{p3}{p4}{p5}"
 
-    # p1 = "magica/resource/image_web/regularEvent/"
-    # p3 = "/"
-    # p5 = "/story_collection_title.png"
-    # for i in ("accomplish", "extermination"):
-    #     p2 = i
-    #     for j in range(1900, 2101):
-    #         p4 = str(j).zfill(4)
-    #         yield f"{p1}{p2}{p3}{p4}{p5}"
+    p1 = "magica/resource/image_web/regularEvent/"
+    p3 = "/"
+    p5 = "/story_collection_title.png"
+    for i in ("accomplish", "extermination"):
+        p2 = i
+        for j in range(1900, 2101):
+            p4 = str(j).zfill(4)
+            yield f"{p1}{p2}{p3}{p4}{p5}"
 
     p1 = "magica/resource/image_web/regularEvent/groupBattle/"
     p3 = "/story_collection_title.png"
