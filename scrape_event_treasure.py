9b4a057f83439d9c5175140194e6a969997bc99d
Alyna Gray <alynagray1@gmail.com>
2024-10-13 21:40:03 -0700

    Fix scrape event treasure
    It wasn't actually trying to scrape without replacing regularEvent
    because it was reusing p2 from the last loop.

--- scrape_event_treasure.py
+++ scrape_event_treasure.py
@@ -15,15 +15,14 @@ def candidates() -> Generator[str, None, None]:
             yield f"{p1}{p2}{p3}"
 
     for dirpath, _, _ in os.walk("magica/resource/image_web/regularEvent"):
-        p2 = dirpath.removeprefix("magica/resource/image_web/")
         for i in ("event", "regularEvent"):
+            p2 = dirpath.removeprefix("magica/resource/image_web/")
             p2 = p2.replace("regularEvent", i)
             p2 = p2.replace(os.path.sep, "_")
             p2 = p2.lower()
             for j in ("_key_close.png", "_key_open.png"):
                 p3 = j
                 yield f"{p1}{p2}{p3}"
-
 
 def main():
     common.scrape_all(candidates)
