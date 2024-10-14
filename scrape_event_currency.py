42a519244da263c2d3dc0a86f4c71b60d48d1699
Alyna Gray <alynagray1@gmail.com>
2024-10-13 19:08:38 -0700

    Expend scrape event currency

--- scrape_event_currency.py
+++ scrape_event_currency.py
@@ -11,7 +11,7 @@ def candidates() -> Generator[str, None, None]:
         p2 = dirpath.removeprefix("magica/resource/image_web/")
         p2 = p2.replace(os.path.sep, "_")
         p2 = p2.lower()
-        for i in ("", "_exchange_1", "_exchange_2", "_exchange_destiny", "_coin", "_part_1", "_part_2", "_key", "_potion"):
+        for i in ("", "_exchange_1", "_exchange_2", "_exchange_3", "_exchange_4", "_exchange_5", "_exchange_destiny", "_coin", "_part_1", "_part_2", "_key", "_potion", "_key_open", "_key_close", "_atkup", "_defup", "_hpup", "_cure_cp"):
             p3 = i
             yield f"{p1}{p2}{p3}{p4}"
 
@@ -20,7 +20,7 @@ def candidates() -> Generator[str, None, None]:
         p2 = p2.replace("regularEvent", "event")
         p2 = p2.replace(os.path.sep, "_")
         p2 = p2.lower()
-        for i in ("", "_exchange_1", "_exchange_2", "_exchange_destiny", "_coin", "_part_1", "_part_2", "_key", "_potion"):
+        for i in ("", "_exchange_1", "_exchange_2", "_exchange_3", "_exchange_4", "_exchange_5", "_exchange_destiny", "_coin", "_part_1", "_part_2", "_key", "_potion", "_key_open", "_key_close", "_atkup", "_defup", "_hpup", "_cure_cp"):
             p3 = i
             yield f"{p1}{p2}{p3}{p4}"
 
