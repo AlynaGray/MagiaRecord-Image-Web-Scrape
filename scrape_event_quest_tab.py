2753fdfed5b3c30fa2dbe91d4a2486f6d489e9a4
Alyna Gray <alynagray1@gmail.com>
2024-11-17 13:25:01 -0800

    Expand scrape event quest tab
    Didn't find any new files.

--- scrape_event_quest_tab.py
+++ scrape_event_quest_tab.py
@@ -9,7 +9,16 @@ def candidates() -> Generator[str, None, None]:
     p5 ="/tab_limited_quest_s.png"
     for i in ("accomplish", "aprilfool2018", "arenaMission", "arenaRankMatch", "arenaranking", "branch", "dailytower", "dungeon", "eventWalpurgis", "eventWitch", "raid", "singleraid", "storyraid", "tower", "training"):
         p2 = i
-        for j in range(1000, 1401):
+        for j in range(10000):
+            p4 = str(j).zfill(4)
+            yield f"{p1}{p2}{p3}{p4}{p5}"
+
+    p1 = "magica/resource/image_web/regularEvent/"
+    p3 = "/"
+    p5 ="/tab_limited_quest_s.png"
+    for i in ("accomplish", "extermination", "groupBattle"):
+        p2 = i
+        for j in range(10000):
             p4 = str(j).zfill(4)
             yield f"{p1}{p2}{p3}{p4}{p5}"
 
